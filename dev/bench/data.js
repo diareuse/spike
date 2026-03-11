61273fdcbf7ae5fc60efa9b3cc458f7b8e1a0597
github-action-benchmark <github@users.noreply.github.com>
2026-03-11 17:37:40 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 317f487b4db4af90c18883f303d1b1a5f12ed936

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773250660725,
+  "repoUrl": "https://github.com/diareuse/spike",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hello@depasquale.wiki",
+            "name": "Viktor De Pasquale",
+            "username": "diareuse"
+          },
+          "committer": {
+            "email": "hello@depasquale.wiki",
+            "name": "Viktor De Pasquale",
+            "username": "diareuse"
+          },
+          "distinct": true,
+          "id": "317f487b4db4af90c18883f303d1b1a5f12ed936",
+          "message": "chore(benchmark): add git stash step before result generation\n\n- Added `git stash` command in the benchmark workflow to prevent uncommitted changes from interfering with subsequent steps.\n- Ensures clean state before generating `results.json`.",
+          "timestamp": "2026-03-11T18:30:35+01:00",
+          "tree_id": "2b16a4b31b180d6f6d60d8393ea7f44a220337f1",
+          "url": "https://github.com/diareuse/spike/commit/317f487b4db4af90c18883f303d1b1a5f12ed936"
+        },
+        "date": 1773250659845,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build Time",
+            "value": 2651.96,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
