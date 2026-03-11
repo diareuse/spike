6af4bc416724ea811ff948239b6bfa750744e624
github-action-benchmark <github@users.noreply.github.com>
2026-03-11 18:12:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8244fb303fcda939d7ed7ddf1b52c6d83c859559

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773250660725,
+  "lastUpdate": 1773252742920,
   "repoUrl": "https://github.com/diareuse/spike",
   "entries": {
     "Benchmark": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Build Time",
             "value": 2651.96,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hello@depasquale.wiki",
+            "name": "Viktor De Pasquale",
+            "username": "diareuse"
+          },
+          "committer": {
+            "email": "hello@depasquale.wiki",
+            "name": "Viktor De Pasquale",
+            "username": "diareuse"
+          },
+          "distinct": true,
+          "id": "8244fb303fcda939d7ed7ddf1b52c6d83c859559",
+          "message": "docs: add build status badge to README\n\n- Added a static badge showing build status to the README file.\n- Links to benchmarking history for more detailed insights.",
+          "timestamp": "2026-03-11T19:05:39+01:00",
+          "tree_id": "a7593a03070826ac021a8f3cc4ab35e7c2d1b293",
+          "url": "https://github.com/diareuse/spike/commit/8244fb303fcda939d7ed7ddf1b52c6d83c859559"
+        },
+        "date": 1773252742469,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build Time",
+            "value": 2720.32,
             "unit": "ms"
           }
         ]
