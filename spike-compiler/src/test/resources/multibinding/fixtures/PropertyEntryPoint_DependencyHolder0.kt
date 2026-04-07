bd2d53e18be6a13c24e963848ec58ae56d470f7b
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-07 16:35:50 +0200

    chore: update fixtures

---
+++ spike-compiler/src/test/resources/multibinding/fixtures/PropertyEntryPoint_DependencyHolder0.kt
@@ -0,0 +1,25 @@
+import kotlin.Any
+import kotlin.Array
+import kotlin.Int
+import kotlin.String
+import kotlin.collections.listOf
+import kotlin.collections.mapOf
+import kotlin.collections.setOf
+
+public object PropertyEntryPoint_DependencyHolder0 {
+  internal fun create(buffer: Array<Any?>, position: Int): Any = when(position) {
+    0 -> mapOf<String, Cat>(
+      "sphynx" to buffer[0] as Cat,
+      "ragamuffin" to buffer[1] as Cat
+    )
+    1 -> ragamuffin()
+    2 -> sphynx()
+    3 -> listOf(buffer[0] as Cat, buffer[1] as Cat)
+    4 -> bombay()
+    5 -> toyger()
+    6 -> setOf(buffer[0] as Cat, buffer[1] as Cat)
+    7 -> abyssinian()
+    8 -> ragdoll()
+    else -> error("Invalid position")
+  }
+}
