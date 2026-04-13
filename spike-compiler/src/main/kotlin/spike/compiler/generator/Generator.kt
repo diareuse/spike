cc68dd0e66e757d7b7834239de8a4db4b7cba5f6
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-13 18:43:05 +0200

    chore(compiler): remove deprecated type generators
    - Deleted outdated container and entry point type generators.
    - Removed redundant invocation generator classes for improved maintainability.
    - Cleaned up unused type generation logic and related dependencies.

--- spike-compiler/src/main/kotlin/spike/compiler/generator/Generator.kt
+++ spike-compiler/src/main/kotlin/spike/compiler/generator/Generator.kt
@@ -4,10 +4,6 @@ import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.TypeSpec
 
-typealias FileGenerator<T> = Generator<T, FileSpec.Builder>
-typealias TypeGenerator<T> = Generator<T, TypeSpec.Builder>
-typealias CodeBlockGenerator<T> = Generator<T, CodeBlock.Builder>
-
 interface Generator<Subject, Builder> {
     fun generate(chain: GeneratorChain<Subject, Builder>): Builder
 }
