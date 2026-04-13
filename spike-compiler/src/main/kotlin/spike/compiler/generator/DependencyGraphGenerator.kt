948c8f22195d2eba3064f02e37324bb0970e3c96
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-13 18:43:05 +0200

    chore(compiler): remove deprecated type generators
    - Deleted outdated container and entry point type generators.
    - Removed redundant invocation generator classes for improved maintainability.
    - Cleaned up unused type generation logic and related dependencies.

--- spike-compiler/src/main/kotlin/spike/compiler/generator/DependencyGraphGenerator.kt
+++ spike-compiler/src/main/kotlin/spike/compiler/generator/DependencyGraphGenerator.kt
@@ -13,66 +13,6 @@ class DependencyGraphGenerator(
     private val resolver = TypeResolver()
 
     fun generate(graph: DependencyGraph) {
-        /*val dependencyContainerType = GeneratorChain.classType(
-            subject = graph,
-            resolver = resolver,
-            name = resolver.getDependencyContainerClassName(graph.entry.type),
-            DependencyContainerTypeConstructor(),
-            DependencyContainerTypeFactory(),
-            DependencyContainerTypeInternal(),
-        )
-        val dependencyContainerFile = GeneratorChain.file(
-            subject = graph,
-            resolver = resolver,
-            name = resolver.getDependencyContainerClassName(graph.entry.type),
-            type = dependencyContainerType,
-        )
-        try {
-            dependencyContainerFile.proceed().build().writeTo(environment.codeGenerator, false)
-        } catch (_: FileAlreadyExistsException) {
-        }
-
-        val entryPointFactoryType = GeneratorChain.objectType(
-            subject = graph.entry.factory,
-            resolver = resolver,
-            name = resolver.transformClassName(graph.entry.factory.type),
-            EntryPointFactoryTypeSuperinterface(),
-            EntryPointFactoryTypeInternal(),
-            EntryPointFactoryTypeMethod(),
-        )
-        val entryPointFactoryFile = GeneratorChain.file(
-            subject = graph.entry.factory,
-            resolver = resolver,
-            name = resolver.transformClassName(graph.entry.factory.type),
-            type = entryPointFactoryType,
-        )
-        try {
-            entryPointFactoryFile.proceed().build().writeTo(environment.codeGenerator, false)
-        } catch (_: FileAlreadyExistsException) {
-        }
-
-        val entryPointType = GeneratorChain.classType(
-            subject = graph.entry,
-            resolver = resolver,
-            name = resolver.transformClassName(graph.entry.type),
-            EntryPointTypeInternal(),
-            EntryPointTypeSuperinterface(),
-            EntryPointTypeConstructor(),
-            EntryPointTypeProperties(),
-            EntryPointTypeFunctions(),
-        )
-        val entryPointFile = GeneratorChain.file(
-            subject = graph.entry,
-            resolver = resolver,
-            name = resolver.transformClassName(graph.entry.type),
-            type = entryPointType,
-            EntryPointFileFactoryAccessor(),
-            EntryPointFileInitializer(),
-        )
-        try {
-            entryPointFile.proceed().build().writeTo(environment.codeGenerator, false)
-        } catch (_: FileAlreadyExistsException) {
-        }*/
         MegaGenerator(graph, resolver).generate().forEach { file ->
             try {
                 file.writeTo(environment.codeGenerator, false)
