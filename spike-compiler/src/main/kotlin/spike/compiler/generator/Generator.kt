3790407432ba3b7f7e07a136377fba6fb97bcecd
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-13 19:24:45 +0200

    chore(compiler): remove unused generator classes
    - Deleted `Generator`, `GeneratorChain`, `GeneratorChainOrigin`, and related classes.
    - Removed `GeneratorFileWithType` implementation and references.
    - Updated `SpikeSymbolProcessor` to use simplified logic for `GraphContributorEntryPoint`.

--- spike-compiler/src/main/kotlin/spike/compiler/generator/Generator.kt
+++
@@ -1,9 +0,0 @@
-package spike.compiler.generator
-
-import com.squareup.kotlinpoet.CodeBlock
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.TypeSpec
-
-interface Generator<Subject, Builder> {
-    fun generate(chain: GeneratorChain<Subject, Builder>): Builder
-}
