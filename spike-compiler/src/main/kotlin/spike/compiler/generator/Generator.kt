a3510d7165103086620e130ff2dfa7873b137e01
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-16 19:14:50 +0200

    refactor(compiler): extract DependencyFactory logic to dedicated generator
    - Introduced `DependencyFactoryGenerator` to manage Factory generation logic.
    - Removed factory-related code from `MegaGenerator` for modularity.
    - Updated `DependencyGraphGenerator` to integrate `DependencyFactoryGenerator`.
    - Refactored `Generator` interface to support `FileSpecCollector` for file emission.
    - Enhanced maintainability and decoupled responsibilities across generators.

--- spike-compiler/src/main/kotlin/spike/compiler/generator/Generator.kt
+++ spike-compiler/src/main/kotlin/spike/compiler/generator/Generator.kt
@@ -10,13 +10,22 @@ import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.asTypeName
+import com.squareup.kotlinpoet.withIndent
 import spike.compiler.graph.DependencyGraph
 import spike.compiler.graph.TypeFactory
+import spike.compiler.graph.TypeFactory.Companion.dependencyTree
+import spike.compiler.graph.TypeFactory.Companion.invertDependencyTree
+import spike.factory.DependencyFactory
 import spike.factory.DependencyId
 import spike.factory.InstructionSet
+import spike.factory.InstructionSetPointer
 
 fun interface Generator {
-    fun generate(context: FileGeneratorContext): FileSpec
+    fun generate(context: FileGeneratorContext, collector: FileSpecCollector)
+}
+
+fun interface FileSpecCollector {
+    fun emit(file: FileSpec)
 }
 
 data class FileGeneratorContext(
@@ -33,7 +42,7 @@ data class FileGeneratorContext(
 class EntryPointGenerator(
     private val dependencyFactoryClassName: ClassName
 ) : Generator {
-    override fun generate(context: FileGeneratorContext): FileSpec {
+    override fun generate(context: FileGeneratorContext, collector: FileSpecCollector) {
         val graph = context.graph
         val resolver = context.resolver
         val ep = graph.entry
@@ -63,7 +72,7 @@ class EntryPointGenerator(
                     .build()
             )
         }
-        return FileSpec.builder(epcn)
+        val file = FileSpec.builder(epcn)
             .addType(type.build())
             .addFunction(
                 FunSpec.builder("invoke")
@@ -74,11 +83,12 @@ class EntryPointGenerator(
                     .build()
             )
             .build()
+        collector.emit(file)
     }
 }
 
 class InstructionSetGenerator : Generator {
-    override fun generate(context: FileGeneratorContext): FileSpec {
+    override fun generate(context: FileGeneratorContext, collector: FileSpecCollector) {
         val instructionSetClassName = context.resolver.peerClass(context.graph, "InstructionSet")
         val dfis = context.instructions
         val type = TypeSpec.objectBuilder(instructionSetClassName)
@@ -110,9 +120,10 @@ class InstructionSetGenerator : Generator {
             blockIndex++
         }
         type.addInitializerBlock(initializer.build())
-        return FileSpec.builder(instructionSetClassName)
+        val file = FileSpec.builder(instructionSetClassName)
             .addType(type.build())
             .build()
+        collector.emit(file)
     }
 }
 
@@ -120,14 +131,15 @@ class DependencyHolderGenerator(
     private val index: Int,
     private val dependencyFactoryClassName: ClassName
 ) : Generator {
-    override fun generate(context: FileGeneratorContext): FileSpec {
+    override fun generate(context: FileGeneratorContext, collector: FileSpecCollector) {
         val factories = context.ids.toList()[index]
         val className = context.resolver.peerClass(context.graph, "DependencyHolder${index}")
         val type = TypeSpec.objectBuilder(className)
         type.addFunction(context.run { createCreateMethod(factories) })
-        return FileSpec.builder(className)
+        val file = FileSpec.builder(className)
             .addType(type.build())
             .build()
+        collector.emit(file)
     }
 
     context(context: FileGeneratorContext)
@@ -171,5 +183,129 @@ class DependencyHolderGenerator(
         builder.addCode(body.build())
 
         return builder.build()
+    }
+}
+
+class DependencyFactoryGenerator(
+    private val dependencyFactoryClassName: ClassName,
+    private val instructionSet: InstructionSetGenerator,
+    private val dependencyHolder: (Int) -> DependencyHolderGenerator,
+) : Generator {
+    override fun generate(context: FileGeneratorContext, collector: FileSpecCollector) {
+        val spec = TypeSpec.objectBuilder(dependencyFactoryClassName)
+        spec.superclass(DependencyFactory::class)
+        val graph = context.graph
+        val maxConstructorArgs = graph.toSequence().flatMap { it.dependencyTree() }.maxOf { it.dependencies.size }
+        spec.addProperty(
+            PropertySpec.builder("maxConstructorArgs", Int::class)
+                .initializer("$maxConstructorArgs")
+                .addModifiers(KModifier.OVERRIDE)
+                .build()
+        )
+        // ---
+        spec.addFunction(
+            FunSpec.builder("getInstructionsPointer")
+                .addModifiers(KModifier.OVERRIDE)
+                .returns(InstructionSetPointer::class.asClassName().copy(nullable = true))
+                .addParameter("id", DependencyId::class)
+                .addCode(createGetInstructionsBody(context))
+                .build()
+        )
+        spec.addFunction(
+            FunSpec.builder("instantiate")
+                .addModifiers(KModifier.OVERRIDE)
+                .returns(Any::class)
+                .addParameter("buffer", Array::class.asClassName().parameterizedBy(Any::class.asTypeName().copy(nullable = true)))
+                .addParameter("id", DependencyId::class)
+                .addCode(createInstantiateBody(context, collector))
+                .build()
+        )
+        spec.addProperty(
+            PropertySpec.builder("instructionSet", IntArray::class)
+                .addModifiers(KModifier.OVERRIDE)
+                .getter(
+                    FunSpec.getterBuilder()
+                        .addStatement("return %T.%L", createInstructionSet(context, collector), InstructionSet::memory.name)
+                        .build()
+                )
+                .build()
+        )
+        val type = spec.build()
+        val file = FileSpec.builder(dependencyFactoryClassName)
+            .addType(type)
+            .build()
+        collector.emit(file)
+    }
+
+    private fun createInstantiateBody(context: FileGeneratorContext, collector: FileSpecCollector): CodeBlock {
+        val block = CodeBlock.builder()
+        block.beginControlFlow("return when (id.%L) {", DependencyId::segment.name)
+        block.withIndent {
+            for (index in context.ids.indices) {
+                val holder = buildList {
+                    dependencyHolder(index).generate(context) {
+                        collector.emit(it)
+                        add(it)
+                    }
+                }.single().toClassName()
+                addStatement("$index -> %T.create(buffer, id.position)", holder)
+            }
+            addStatement("else -> error(\"Invalid segment\")")
+        }
+        block.endControlFlow()
+        return block.build()
+    }
+
+    private fun createGetInstructionsBody(context: FileGeneratorContext): CodeBlock {
+        val graph = context.graph
+        val block = CodeBlock.builder()
+            .beginControlFlow("return when (id.%L) {", DependencyId::id.name)
+        val queue = ArrayDeque(graph.toSequence().toList())
+        val uniqueTypes = HashSet<Int>()
+        while (queue.isNotEmpty()) {
+            val type = queue.removeFirst()
+            if (!uniqueTypes.add(type.hashCode())) {
+                continue
+            }
+            val contextIndexByHash = HashMap<Int, Int>()
+            block.add("%L -> ", context.getDependencyId(type))
+            val offset = context.instructions.start()
+            var contextSize = 0
+            for (dependency in type.invertDependencyTree()) {
+                when (dependency) {
+                    is TypeFactory.Deferred -> queue.add(0, dependency.factory)
+                    is TypeFactory.MultibindsCollection -> queue.addAll(0, dependency.entries)
+                    is TypeFactory.MultibindsMap -> queue.addAll(0, dependency.keyValues.values)
+                    else -> {}
+                }
+                val dependencyHash = dependency.hashCode()
+                if (contextIndexByHash.containsKey(dependencyHash))
+                    continue
+                contextIndexByHash[dependencyHash] = contextSize++
+                val id = context.getDependencyId(dependency)
+                context.instructions.add(id)
+                val dependencyCount = if (dependency is TypeFactory.Deferred) 0 else dependency.dependencies.size
+                context.instructions.add(dependencyCount)
+                if (dependency !is TypeFactory.Deferred) for (argument in dependency.dependencies) {
+                    val i = contextIndexByHash[argument.hashCode()]
+                        ?: error("Dependency '$argument' was not found in dependencyTree")
+                    context.instructions.add(i)
+                }
+            }
+            val size = context.instructions.end(contextSize)
+            block.addStatement("%T(%L, %L)", InstructionSetPointer::class.asClassName(), offset, size)
+        }
+        block.addStatement($$"else -> error(\"Invalid identifier ${id}\")")
+        block.endControlFlow()
+        return block.build()
+    }
+
+    private fun createInstructionSet(context: FileGeneratorContext, collector: FileSpecCollector): ClassName {
+        return buildList {
+            instructionSet.generate(context) {
+                collector.emit(it)
+                add(it)
+            }
+        }.single().toClassName()
     }
 }
--- spike-compiler/src/main/kotlin/spike/compiler/generator/MegaGenerator.kt
+++ spike-compiler/src/main/kotlin/spike/compiler/generator/MegaGenerator.kt
@@ -1,155 +1,17 @@
 package spike.compiler.generator
 
-import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.KModifier
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.PropertySpec
-import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.asClassName
-import com.squareup.kotlinpoet.asTypeName
-import com.squareup.kotlinpoet.withIndent
-import spike.compiler.graph.DependencyGraph
-import spike.compiler.graph.TypeFactory
-import spike.compiler.graph.TypeFactory.Companion.dependencyTree
-import spike.compiler.graph.TypeFactory.Companion.invertDependencyTree
-import spike.factory.DependencyFactory
-import spike.factory.DependencyId
-import spike.factory.InstructionSet
-import spike.factory.InstructionSetPointer
 
 class MegaGenerator(
     private val context: FileGeneratorContext,
     private val entryPoint: EntryPointGenerator,
-    private val instructionSet: InstructionSetGenerator,
-    private val dependencyHolder: (Int) -> DependencyHolderGenerator,
+    private val dependencyFactory: DependencyFactoryGenerator
 ) {
-    private val graph: DependencyGraph inline get() = context.graph
-    private val resolver: TypeResolver inline get() = context.resolver
 
-    private val dependencyFactoryClassName = resolver.peerClass(graph, "Factory")
-    private val dfis inline get() = context.instructions
-
-    private val types = mutableListOf<FileSpec>()
-
-    fun generate(): List<FileSpec> {
-        types.clear()
-        types += createDependencyFactory()
-        types += entryPoint.generate(context)
-        return types.toList()
+    fun generate(): List<FileSpec> = buildList {
+        dependencyFactory.generate(context, ::add)
+        entryPoint.generate(context, ::add)
     }
 
-    private fun createDependencyFactory(): FileSpec {
-        val spec = TypeSpec.objectBuilder(dependencyFactoryClassName)
-        spec.superclass(DependencyFactory::class)
-        val maxConstructorArgs = graph.toSequence().flatMap { it.dependencyTree() }.maxOf { it.dependencies.size }
-        spec.addProperty(
-            PropertySpec.builder("maxConstructorArgs", Int::class)
-                .initializer("$maxConstructorArgs")
-                .addModifiers(KModifier.OVERRIDE)
-                .build()
-        )
-        // ---
-        spec.addFunction(
-            FunSpec.builder("getInstructionsPointer")
-                .addModifiers(KModifier.OVERRIDE)
-                .returns(InstructionSetPointer::class.asClassName().copy(nullable = true))
-                .addParameter("id", DependencyId::class)
-                .addCode(createGetInstructionsBody(graph))
-                .build()
-        )
-        spec.addFunction(
-            FunSpec.builder("instantiate")
-                .addModifiers(KModifier.OVERRIDE)
-                .returns(Any::class)
-                .addParameter("buffer", Array::class.asClassName().parameterizedBy(Any::class.asTypeName().copy(nullable = true)))
-                .addParameter("id", DependencyId::class)
-                .addCode(createInstantiateBody())
-                .build()
-        )
-        spec.addProperty(
-            PropertySpec.builder("instructionSet", IntArray::class)
-                .addModifiers(KModifier.OVERRIDE)
-                .getter(
-                    FunSpec.getterBuilder()
-                        .addStatement("return %T.%L", createInstructionSet(), InstructionSet::memory.name)
-                        .build()
-                )
-                .build()
-        )
-        val type = spec.build()
-        return FileSpec.builder(dependencyFactoryClassName)
-            .addType(type)
-            .build()
-    }
-
-    // ---
-    private fun createInstantiateBody(): CodeBlock {
-        val block = CodeBlock.builder()
-        block.beginControlFlow("return when (id.%L) {", DependencyId::segment.name)
-        block.withIndent {
-            for (index in context.ids.indices) {
-                val holder = dependencyHolder(index).generate(context).also { types += it }.toClassName()
-                addStatement("$index -> %T.create(buffer, id.position)", holder)
-            }
-            addStatement("else -> error(\"Invalid segment\")")
-        }
-        block.endControlFlow()
-        return block.build()
-    }
-
-    private fun createGetInstructionsBody(graph: DependencyGraph): CodeBlock {
-        val block = CodeBlock.builder()
-            .beginControlFlow("return when (id.%L) {", DependencyId::id.name)
-        val queue = ArrayDeque(graph.toSequence().toList())
-        val uniqueTypes = HashSet<Int>()
-        while (queue.isNotEmpty()) {
-            val type = queue.removeFirst()
-            if (!uniqueTypes.add(type.hashCode())) {
-                continue
-            }
-            val contextIndexByHash = HashMap<Int, Int>()
-            block.add("%L -> ", getDependencyId(type))
-            val offset = dfis.start()
-            var contextSize = 0
-            for (dependency in type.invertDependencyTree()) {
-                when (dependency) {
-                    is TypeFactory.Deferred -> queue.add(0, dependency.factory)
-                    is TypeFactory.MultibindsCollection -> queue.addAll(0, dependency.entries)
-                    is TypeFactory.MultibindsMap -> queue.addAll(0, dependency.keyValues.values)
-                    else -> {}
-                }
-                val dependencyHash = dependency.hashCode()
-                if (contextIndexByHash.containsKey(dependencyHash))
-                    continue
-                contextIndexByHash[dependencyHash] = contextSize++
-                val id = getDependencyId(dependency)
-                dfis.add(id)
-                val dependencyCount = if (dependency is TypeFactory.Deferred) 0 else dependency.dependencies.size
-                dfis.add(dependencyCount)
-                if (dependency !is TypeFactory.Deferred) for (argument in dependency.dependencies) {
-                    val i = contextIndexByHash[argument.hashCode()]
-                        ?: error("Dependency '$argument' was not found in dependencyTree")
-                    dfis.add(i)
-                }
-            }
-            val size = dfis.end(contextSize)
-            block.addStatement("%T(%L, %L)", InstructionSetPointer::class.asClassName(), offset, size)
-        }
-        block.addStatement($$"else -> error(\"Invalid identifier ${id}\")")
-        block.endControlFlow()
-        return block.build()
-    }
-
-    // ---
-    private fun getDependencyId(factory: TypeFactory): Int = context.getDependencyId(factory)
-
-    // ---###---
-
-    private fun createInstructionSet(): ClassName {
-        return instructionSet.generate(context).also { types += it }.toClassName()
-    }
 
 }
