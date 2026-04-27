82b57efe12c1f2e1eafe96fcb7e3afdcd9b7d6cc
Viktor De Pasquale <hello@depasquale.wiki>
2026-04-27 14:36:25 +0200

    chore: remove lint

--- spike-compiler/src/main/kotlin/spike/compiler/processor/util/KSAnnotated.kt
+++ spike-compiler/src/main/kotlin/spike/compiler/processor/util/KSAnnotated.kt
@@ -3,10 +3,13 @@ package spike.compiler.processor.util
 import com.google.devtools.ksp.symbol.KSAnnotated
 import kotlin.reflect.KProperty1
 
-inline fun <reified A, reified P> KSAnnotated.getAnnotationParameterOrDefault(parameter: KProperty1<A, Any?>, default: P): P {
+inline fun <reified A, reified P> KSAnnotated.getAnnotationParameterOrDefault(
+    parameter: KProperty1<A, Any?>,
+    default: P
+): P {
     val annotation = annotations.singleOrNull {
         it.annotationType.resolve().declaration.qualifiedName?.asString() == A::class.qualifiedName
-    } ?: return default
-    val arg = annotation.arguments.singleOrNull { it.name?.asString() == parameter.name } ?: return default
+    }
+    val arg = annotation?.arguments?.singleOrNull { it.name?.asString() == parameter.name } ?: return default
     return arg.value as P
 }
