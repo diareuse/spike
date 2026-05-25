66be40b39430bef7e22addf25883ec48ba319d5e
Viktor De Pasquale <hello@depasquale.wiki>
2026-05-25 15:35:53 +0200

    fix(core): add delegate support to Provider interface
    Added getValue operator function to Provider companion object enabling delegate property support. This allows Provider instances to be used as delegates in Kotlin properties, providing a more idiomatic way to access provided values.
    
    The change introduces kotlin.reflect.KProperty dependency and implements the getValue contract to retrieve values from providers, making the Provider interface more versatile for property delegation scenarios.
    ```

--- spike-core/src/commonMain/kotlin/spike/Provider.kt
+++ spike-core/src/commonMain/kotlin/spike/Provider.kt
@@ -1,9 +1,12 @@
 package spike
 
+import kotlin.reflect.KProperty
+
 public fun interface Provider<T> {
     public fun get(): T
 
     public companion object {
         public operator fun <T> Provider<T>.invoke(): T = get()
+        public operator fun <T> Provider<T>.getValue(any: Any?, property: KProperty<*>): T = get()
     }
 }
